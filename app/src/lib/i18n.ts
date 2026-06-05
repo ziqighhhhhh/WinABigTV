84177289262422192419ea72a4276ae206795581
ziqighhhhhh <578027072@qq.com>
2026-06-05 12:11:12 +0800

    fix: use i18next-http-backend for locale loading

--- app/src/lib/i18n.ts
+++ app/src/lib/i18n.ts
@@ -1,30 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-import enTranslation from '../public/locales/en/translation.json';
-import arTranslation from '../public/locales/ar/translation.json';
-import frTranslation from '../public/locales/fr/translation.json';
+import HttpApi from 'i18next-http-backend';
 
 i18n
+  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: enTranslation,
-      },
-      ar: {
-        translation: arTranslation,
-      },
-      fr: {
-        translation: frTranslation,
-      },
-    },
     fallbackLng: 'en',
     detection: {
       order: ['navigator', 'htmlTag'],
       caches: [],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
     },
     interpolation: {
       escapeValue: false,
